ee55940db651ee3737ca0c39df1181d9fc322c4d
KarthikPisharody <karthikpisharody7284@gmail.com>
2025-11-23 22:08:24 +0530

    added express server

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
-import { config } from "dotenv";
+import { config } from 'dotenv';
 config({ debug: true });
 
 const { PORT, NODE_ENV } = process.env;
 
 export const Config = {
-    PORT,
-    NODE_ENV,
+  PORT,
+  NODE_ENV,
 };
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
-import { Config } from "./config";
+import { Config } from './config';
+import app from './config/app';
 
-console.log(Config.PORT);
+const startServer = () => {
+  const PORT = Config.PORT;
+
+  try {
+    app.listen(PORT, () => {
+      console.log(`Auth service is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+    console.log(1);
+  }
+};
+startServer();
