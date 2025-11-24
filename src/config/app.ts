16e48a2c134861b523ca86570a6be30d4726259a
KarthikPisharody <karthikpisharody7284@gmail.com>
2025-11-24 20:41:49 +0530

    set up error handling

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,9 +1,30 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import logger from './logger';
+import { HttpError } from 'http-errors';
 
 export const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Welcome to auth service');
+  res.send('Hello , welcome to the server');
 });
+
+// global error handler
+app.use(
+  // eslint-disable-next-line no-unused-vars
+  (err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+      errors: [
+        {
+          type: err.name,
+          message: err.message,
+          path: '',
+          location: '',
+        },
+      ],
+    });
+  },
+);
 
 export default app;
